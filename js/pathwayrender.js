8564eb278d301ff5673e2e0c794b457d69105904
jVillaveces <secevalliv@gmail.com>
2016-01-22 13:16:29 +0100

    porcessing edge without jquery

--- js/pathwayrender.js
+++ js/pathwayrender.js
@@ -1,5 +1,4 @@
 //Libraries
-var $ = require('jquery');
 var cytoscape = require('cytoscape');
 
 //Variables
@@ -36,8 +35,7 @@ function _processEntry(entry){
     }else if(type == 'map'){
         node.shape = 'roundrectangle';
         node.bkg_color = '#00bfff';
-    }else if( type == 'group'){
-        
+    }else if( type == 'group'){    
         var components = entry.getElementsByTagName('component');
         for(var i = 0; i < components.length; i++){
             node_map[components[i].getAttribute('id')].data.parent = node.id;
@@ -53,139 +51,78 @@ function _processEntry(entry){
     };
 };
 
-
+function _processRelation(rel){
+    var type = rel.getAttribute('type'), subtypes = [];
+    
+    var subs = rel.getElementsByTagName('subtype');
+    for(var i=0; i<subs.length; i++){
+        var sub = subs[i];
+        
+        var edge = {
+            source: rel.getAttribute('entry1'),
+            target: rel.getAttribute('entry2'),
+            name: sub.getAttribute('name'),
+            reaction: type,
+            line_style: 'solid',
+            target_arrow_shape: 'none',
+            text: ''
+        };
+        
+        if(edge.name == 'maplink'){
+            edge.target_arrow_shape = 'diamond';
+        }else if(edge.name == 'indirect effect'){
+            edge.line_style = 'dotted';
+            edge.target_arrow_shape = 'diamond'
+        }else if(edge.name == 'state change'){
+            edge.line_style = 'dotted';
+        }else if(edge.name == 'missing interaction'){
+            edge.line_style = 'dotted';
+            edge.target_arrow_shape = 'triangle';
+        }else if(edge.name == 'phosphorylation'){
+            edge.target_arrow_shape = 'triangle';
+            edge.text = 'p+';
+        }else if(edge.name == 'dephosphorylation'){
+            edge.target_arrow_shape = 'triangle';
+            edge.text = 'p-';
+        }else if(edge.name == 'glycosylation'){
+            edge.line_style = 'dashed';
+            edge.target_arrow_shape = 'circle';
+        }else if(edge.name == 'ubiquitination'){
+            edge.line_style = 'dashed';
+            edge.target_arrow_shape = 'circle';
+        }else if(edge.name == 'methylation'){
+            edge.line_style = 'dashed';
+            edge.target_arrow_shape = 'circle';
+        }else if(edge.name == 'activation'){
+            edge.target_arrow_shape = 'triangle';
+        }else if(edge.name == 'inhibition'){
+            edge.target_arrow_shape = 'tee';
+        }else if(edge.name == 'expression'){
+            edge.target_arrow_shape = 'triangle';
+        }else if(edge.name == 'repression'){
+            edge.target_arrow_shape = 'tee';
+        }
+        
+        links.push({
+            data:edge
+        });
+        
+    };
+};
 
 var renderPathway = function(data, target){
     
     
     
     var entries = data.getElementsByTagName('entry');
-    
     for(var i = 0; i < entries.length; i++){
         _processEntry(entries[i]);
     }
     
-    /*$(data).find('entry').each(function(){
-        
-        var entry = $(this);
-        var type =  entry.attr('type');
-        var graphics = entry.find('graphics');
-            
-        var text_valign = 'center', 
-            shape = 'rectangle',
-            bkg_color = '#99ff99',
-            opacity = 0.9,
-            border_width = 0,
-            width = $(graphics).attr('width'),
-            height = $(graphics).attr('height');
-            
-        if(type == 'gene'){
-            border_width = 2;
-        }else if(type == 'ortholog'){
-            border_width = 2;
-        }else if(type == 'compound'){
-            shape = 'ellipse';
-            bkg_color = '#aaaaee';
-            text_valign = 'bottom';
-            opacity = 1
-        }else if(type == 'map'){
-            shape = 'roundrectangle';
-            bkg_color = '#00bfff';
-        }else if( type == 'group'){
-            entry.find('component').each(function(){
-                node_map[$(this).attr('id')].data.parent = entry.attr('id');
-            });
-        }
-            
-        var names = [];
-        if(graphics.attr('name') !== undefined){
-            names = graphics.attr('name').split(',');
-        }
-            
-        var node = {};
-        node.data = {
-            id: entry.attr('id'),
-            keggId: entry.attr('name').split(' '),
-            name: names[0],
-            names: names,
-            type: type,
-            link: entry.attr('link'),
-            width: width,
-            height: height,
-            shape: shape,
-            bkg_color: bkg_color,
-            text_valign: text_valign,
-            'border-width': border_width
-        }
-            
-        node_map[entry.attr('id')] = node;
-        nodes.push(node);
-            
-        positions[entry.attr('id')] = {
-            x : +graphics.attr('x'),
-            y : +graphics.attr('y')
-        }
-            
-    });*/
-        
-    $(data).find('relation').each(function(){
-        var rel = $(this), type =  rel.attr('type'), subtypes = [];
-        
-        rel.find('subtype').each(function(){
-            var sub = $(this);
-            var name = sub.attr('name'),
-                line_style = 'solid',
-                target_arrow_shape = 'none',
-                text = '';
-                
-            if(name == 'maplink'){
-                target_arrow_shape = 'diamond';
-            }else if(name == 'indirect effect'){
-                line_style = 'dotted';
-                target_arrow_shape = 'diamond'
-            }else if(name == 'state change'){
-                line_style = 'dotted';
-            }else if(name == 'missing interaction'){
-                line_style = 'dotted';
-                target_arrow_shape = 'triangle';
-            }else if(name == 'phosphorylation'){
-                target_arrow_shape = 'triangle';
-                text = 'p+';
-            }else if(name == 'dephosphorylation'){
-                target_arrow_shape = 'triangle';
-                text = 'p-';
-            }else if(name == 'glycosylation'){
-                line_style = 'dashed';
-                target_arrow_shape = 'circle';
-            }else if(name == 'ubiquitination'){
-                line_style = 'dashed';
-                target_arrow_shape = 'circle';
-            }else if(name == 'methylation'){
-                line_style = 'dashed';
-                target_arrow_shape = 'circle';
-            }else if(name == 'activation'){
-                target_arrow_shape = 'triangle';
-            }else if(name == 'inhibition'){
-                target_arrow_shape = 'tee';
-            }else if(name == 'expression'){
-                target_arrow_shape = 'triangle';
-            }else if(name == 'repression'){
-                target_arrow_shape = 'tee';
-            }
-                
-            links.push({
-                data:{
-                    source: rel.attr('entry1'),
-                    target: rel.attr('entry2'),
-                    name: name,
-                    reaction: type,
-                    line_style: line_style,
-                    target_arrow_shape: target_arrow_shape,
-                    text: text
-                }
-            });
-        });
-    });
+    var rels = data.getElementsByTagName('relation');
+    for(var i = 0; i < rels.length; i++){
+        _processRelation(rels[i]);
+    }
     
     var cy = cytoscape({
         container: target,
